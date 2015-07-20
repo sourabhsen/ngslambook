c72ee59f246fb9cc5ec9cb4562f3137d828dee25
unknown <ShyamS@TECH-LP-242.techaspect.com>
2015-07-21 01:57:26 +0530

    commit

--- js/controllers/signin.js
+++ js/controllers/signin.js
@@ -29,16 +29,32 @@ app.controller('SignupFormController', ['$scope', '$http', '$state','$location',
  
         $scope.signup = function () {
             $scope.dataLoading = true;
+			$scope.userProfile ={
+			          name: $scope.user.name,
+					  password : $scope.user.password,
+					  email    : $scope.user.email
+			 }
             AuthenticationService.validCredentials($scope.user.name, $scope.user.password, $scope.user.email, function(response) {
                 if(response.success) {
                     $scope.authError = response.message;
                     $scope.dataLoading = false;
                 } else {
 					AuthenticationService.SetCredentials($scope.user.name, $scope.user.password,$scope.user.email);
+					
                     $location.path('/access/signin');
+					 response.message = "your profile is created successfully.please signin";
+					$scope.authError = response.message;
 					$scope.dataLoading = false;
                 }
             });
         };
+  }])
+;
+
+
+
+app.controller('headerCntrl', ['$scope','AuthenticationService', function($scope, AuthenticationService) {
+     $scope.currentUser = AuthenticationService.getUserInfo();
+     
   }])
 ;
